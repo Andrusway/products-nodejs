daecd518174a41188f39406724f021fe1059e905
HOG <hogseruj1@gmail.com>
2026-01-31 16:42:08 +0200

    #task-5-2 Created session router.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -8,6 +8,7 @@ import {
   loginUser,
   registerUser,
   logoutUser,
+  refreshSession,
 } from '../controllers/authController.js';
 
 const router = Router();
@@ -15,5 +16,6 @@ const router = Router();
 router.post('/auth/register', celebrate(registerUserValidation), registerUser);
 router.post('/auth/login', celebrate(loginUserValidation), loginUser);
 router.post('/auth/logout', logoutUser);
+router.post('/auth/refresh', refreshSession);
 
 export default router;
