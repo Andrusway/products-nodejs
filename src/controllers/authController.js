643ec53558b88117e53e0b0269d749a55a83cbe9
HOG <hogseruj1@gmail.com>
2026-02-14 15:05:50 +0200

    #task-3-2 request email fn.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,6 +3,11 @@ import { User } from '../models/user.js';
 import bcrypt from 'bcrypt';
 import { setSessionCookie, createSession } from '../services/auth.js';
 import { Session } from '../models/session.js';
+import jwt from 'jsonwebtoken';
+import { sendMail } from '../utils/sendMail.js';
+import handlebars from 'handlebars';
+import path from 'node:path';
+import fs from 'node:fs/promises';
 
 export const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
@@ -79,4 +84,52 @@ export const refreshSession = async (req, res) => {
   setSessionCookie(res, newSession);
 
   res.status(200).json({ message: 'Successfully refreshed a session!' });
+};
+
+export const requestResetEmail = async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw createHttpError(404, 'User not found.');
+  }
+
+  // Create token
+  const resetToken = jwt.sign(
+    { sub: user._id, email },
+    process.env.JWT_SECRET,
+    { expiresIn: '15m' },
+  );
+
+  // Get email template.
+  const templatePath = path.resolve('src/templates/reset-password-email.html');
+  // Template loading.
+  const templateSource = await fs.readFile(templatePath, 'utf-8');
+  // Prepare template.
+  const template = handlebars.compile(templateSource);
+  // Generate HTML content.
+  const html = template({
+    name: user.username,
+    link: `${process.env.FRONTEND_DOMAIN}/reset-password?token=${resetToken}`,
+  });
+
+  // Try to send email if no create an error.
+  try {
+    await sendMail({
+      from: process.env.SMTP_FROM,
+      to: email,
+      subject: 'Reset your password',
+      html,
+    });
+  } catch (error) {
+    console.log(error);
+
+    throw createHttpError(
+      500,
+      'Failed to send the email, please try again later.',
+    );
+  }
+
+  res.status(200).json({
+    message: 'Password reset email sent successfully',
+  });
 };
